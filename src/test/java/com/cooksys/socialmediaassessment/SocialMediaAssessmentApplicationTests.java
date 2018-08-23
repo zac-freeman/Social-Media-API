f043e5751ac30fc686572f01255d17dc58d4fe2d
unknown <zpfreeman14@gmail.com>
2018-08-23 09:30:22 -0500

    first commit

---
+++ src/test/java/com/cooksys/socialmediaassessment/SocialMediaAssessmentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cooksys.socialmediaassessment;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SocialMediaAssessmentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
